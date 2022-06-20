6d883b4d7699c37a8936de9886cdf05cd05fa79d
slopeher <65255181+slopeher@users.noreply.github.com>
2022-06-20 21:03:56 +0200

    Update BucleFor.java

--- BucleFor.java
+++ BucleFor.java
@@ -4,6 +4,11 @@ public class BucleFor {
 	
 	public void bucleFor() {
 	
+		//El bucle for sirve para recorrer arrays o colecciones o para ejecutar una pieza de código mas de una vez
+		//, la estructura es la siguiente:
+		/*
+			for(variable de tipo entero; condición de salida; incremento o decremento){}
+		*/
 		int arrayOMatriz [] = new int[]{1, 2, 2, 3, 2 ,1, 1, 2};
 	
 		for(int i = 0; i < arrayOMatriz.length; i++) {
